0d95585d5da57c21827080ced6839fc66b2cf8ec
MarkyRobertson <mrobertson10001@gmail.com>
2026-09-21 17:41:05 +0100

    Started lab 2, CI Workflow

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -3,5 +3,7 @@ package com.napier.sem;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello world");
+
+        System.out.println("Lab 2 start, CI Workflow");
     }
 }
